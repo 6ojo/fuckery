b22cc9f0a9c26bfa733e932a0cb798d3c1dc303f
gojo <github@gojo.mozmail.com>
2022-11-13 15:29:14 -0700

    disco disco!
    party party!
    https://www.youtube.com/watch?v=ujCnLJ5_9xI&t=124s

---
+++ when i say disco you say party.py
@@ -0,0 +1,32 @@
+import time
+
+def repeat():
+    print('I say disco, you say party')
+    time.sleep(1.8)
+    print('disco')
+    time.sleep(0.4)
+    print('disco')
+    user_input = input('\n')
+    my_str = 'party party'
+    if user_input == my_str:
+        print('\n')
+    else:
+        print('Wrong.')
+        time.sleep(3)
+        print('do better next time')
+        time.sleep(2)
+    return user_input, my_str
+    
+for i in range(2):
+   user_input, my_str=repeat()
+
+
+if user_input == my_str:
+    print('SOLO!')
+    time.sleep(2.3)
+    print('fantastic!')
+    time.sleep(1.2)
+    print('applause!')
+    
+else:
+    print('you missed your chance to solo')
